a0258d613e607c7651d636f406f823298c214bc6
Harshpari Gosai <700091@ODCDPW.IC>
2026-03-11 11:01:12 +0530

    make some changise for backend url add deployement url make it veriable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = "respectful-passion-production.up.railway.app";
 
 export const uploadImage = async (file) => {
   const formData = new FormData();
